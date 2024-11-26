91855ee94621fc4b006554421f37e1247b5388aa
Artur23041989 <danh1989@mail.ru>
2024-11-26 16:33:30 +0300

    new comit in base_1, 2

--- python_base_1.py
+++ python_base_1.py
@@ -1,17 +1,18 @@
-#Типы данных
+# 1. Типы данных:
 
 # Integer (int) - целые числа
 a = 6
 b = 2
 c = a + b
-d = a % b
+d = a / b
+e = a % b
 
 print(c)
 print(d)
-print(b**2)
+print(e)
+print(b**2) # возведение в степень
 
-# Float (float) - вещественные числа
-
+# Float (float) - вещественные числа (числа с плавающей точкой)
 float_a = 0.5
 float_b = 1.25
 
@@ -22,9 +23,10 @@ print(type(float_a))
 is_active = True
 is_logout = False
 
-print(is_active or is_logout)
+print(id(a)) # id - функция, которая показывает адрес ячейки памяти
+print(is_active or is_logout) # or - логическая "или"
 print(is_active and is_logout)
-print(not  is_active and is_logout)
+print(not is_active and is_logout) # not - логическое отрицание
 
 print(a < b)
 print(a > b)
@@ -33,22 +35,22 @@ print(a != b)
 print(a <= b)
 print(a >= b)
 
-
 # приведение строки к типу int
 age = '15'
 year = '2024'
 print(int(age) + int(year))
 
 # String - (str) - строковый тип данных
-
 text = "I 'love' Python!"
+# text = text + " a"
+# text += " a" - тоже самое, что text = text + " a"
 print(text + ' a')
 
 # None
 
 flag = None
 
-# Структура данных
+# 2. Структура данных:
 # списки - list() - []
 
 cars = ['bmw', 'audi', 24, True, [1,2]]
@@ -56,10 +58,10 @@ cars = ['bmw', 'audi', 24, True, [1,2]]
 # словари - dict() - {}
 info = {
     'name': 'Alex',
-    'cars': cars,
+    'cars': cars, # без кавычек пишутся переменные
 }
 
-# кортежи  - tuple - () - неизменяемый список
+# кортежи  - tuple() - () - неизменяемый список. Как создали так и остается, добавить ничего нельзя!
 colors = (
     ('red', '255,0,0'),
     ('blue', '0,0,255')
@@ -67,8 +69,8 @@ colors = (
 
 # set - set() - множества - {}
 
-set_numbers = {1, 2, 3, 4, 5, 5, 5, 5, 5 }
-print(set_numbers)
+set_numbers = {1, 2, 3, 4, 5, 5, 5, 5, 5, 6}
+print(set_numbers) # автоматически удалил дубликаты
 
 # функции
 # файлы
@@ -76,6 +78,13 @@ print(set_numbers)
 name = 5
 Name = 6
 
+'''
+Многострочный коментарий
+ffdfdjdjdkfj
+dfjhfjhdjf
+fjdkfjdjfkl
+fkdjfkjdfkfj
+'''
 
 text_many_lines = """kflkfrk
 rkofkrokfokf
--- python_base_2.py
+++ python_base_2.py
@@ -1,49 +1,50 @@
 # условное выражение
-age = 23
+age = 30
 if age < 20:
     print('Error')
     print('Error')
-    if age < 15:
+    if age < 15: # if - если. Может быть без ветки else. ":" - то
         print('Fatal')
-    else:
+    else: # else - иначе
         print('not fatal')
-elif age < 25:
+elif age < 25: # elif - иначе если, их может быть сколько угодно
     print('<25')
 else:
     print('OK')
 
-
 print('Next')
 
-# цикл FOR
+# цикл for (цыкл счетчиком, безусловный цыкл)
 
 '''numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-for i in numbers:
+for i in numbers: читается для каждого i в коллекции
     print(i)'''
 
 '''numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 N = 10
 
-for i in range(N, 0, -1):
+for i in range(0, N, -1): range - диапазон, -1 - шаг
     print(i)'''
 
 
-#         0       1       2
+#         0       1       2    индексы
 cars = ['bmw', 'audi', 'lada']
 #for car in cars:
 #    print(car)
-for ind in range(len(cars)):
+for ind in range(len(cars)): # len - функция, которая получает количество элементов
 #    print(ind)
 #print(cars[0])
-#print(cars[-2])
+#print(cars[-2]) - получение последнего или предпоследнего и т.п. элемента
 
     print(ind, cars[ind])
 
 a = 10
 b = 0
-while a < 20:
+while a < 20: # читается пока a меньше 20
     print(a)
-    a += 1
+    # a += 1
+    b += 1
+    # if a == 15:
     if b == 15:
         break
 
