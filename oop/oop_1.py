3c43a1929e4fc55e33e8527f0bd43d119938ef67
Artur23041989 <danh1989@mail.ru>
2025-01-14 21:35:32 +0300

    new oop

---
+++ oop/oop_1.py
@@ -0,0 +1,94 @@
+"""
+ООП - объектно-ориентированное программирование
+Класс - общее описание предметной области на языке программирования
+Объект - экземпляр (конкретный представитель класса)
+Метод - функция, связанная с объектом класса (классом)
+Атрибут - характеристика (свойства) объекта или класса
+Конструктор - метод, который управляет созданием объекта
+
+Инкапсуляция - механизм, позволяющий скрывать внутренние детали реализации объекта
+и предоставлять доступ к ним только через определенные методы, чтобы защитить
+данные и контролировать доступ к ним
+"""
+class Car:
+    _COLORS = ("red", "green", "blue", "")
+
+    def __init__(self, brand, model, year, power, currrence="RUB"):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.power = power
+        self.country = "Armenia"
+        self.currence = currrence
+        self.is_power = False
+
+        # защищенный атрибут
+        self._color = ""
+
+        self.__speed = 100
+
+        # метод объекта
+    def go(self):
+        if self.is_power:
+           print(f"{self.brand} {self.model} TO GO!")
+        else:
+            print("Car must be POWER ON")
+
+    def stop(self):
+        if self.is_power:
+            print(f"{self.brand} {self.model} STOP!")
+        else:
+            print("Car must be POWER ON")
+
+    def turn(self, direction):
+        if self.is_power:
+            print(f"{self.brand} {self.model} TURN {direction.upper()}")
+        else:
+            print("Car must be POWER ON")
+
+    def power_on(self):
+        if self.is_power:
+            print("Car already is POWER ON")
+        else:
+            print(f"{self.brand} {self.model} POWER ON!")
+            self.is_power = True
+
+    def power_off(self):
+        if not self.is_power:
+            print("Car already is POWER OFF")
+        else:
+            print(f"{self.brand} {self.model} POWER OFF!")
+            self.is_power = False
+
+    def display_color(self):
+        print(self._color)
+
+    def set_color(self, new_color):
+        if new_color in Car._COLORS:
+            self._color = new_color
+        else:
+            raise ValueError("Неправильный цвет")
+
+
+
+car_audi = Car(brand='Audi', model='A6', year=2022, power=249)
+car_bmw = Car(brand='BMW', model='X5', year=2022, power=333)
+
+car_audi.power_off()
+car_audi.go()
+car_audi.turn(direction="left")
+
+
+car_audi.power_on()
+car_audi.power_on()
+car_audi.go()
+car_audi.turn(direction="left")
+car_audi.power_off()
+
+# car_audi.set_color(new_color="black")
+# car_audi.display_color()
+
+print(dir(car_audi))
+print(car_audi._Car__speed)
+
+
