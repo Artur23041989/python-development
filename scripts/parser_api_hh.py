8a1c906575bbe1009a515848b9420254516a6460
Artur23041989 <danh1989@mail.ru>
2025-01-24 15:27:25 +0300

    new comit parser hh

--- scripts/parser_api_hh.py
+++ scripts/parser_api_hh.py
@@ -1,7 +1,7 @@
 import requests
 
-
-def get_vacancies_data(url: str):
+# функция возвращает количество результатов, страниц
+def get_vacancies_pages(url: str):
     area = 2
     per_page = 100
     text = 'python'
@@ -14,12 +14,38 @@ def get_vacancies_data(url: str):
 
     resp = requests.get(url, params=params)
     data = resp.json()
-    pass
+    return data['pages'], data['found'] # количество найденных страниц и результатов
+
+def get_data_page(url: str, page: int): # получает информацию со странички
+    params = {
+        'area': 2,
+        'per_page': 100,
+        'page': page,
+        'text': 'python'
+    }
+    resp = requests.get(url, params=params)
+    data = resp.json()
+    return data
 
 
 
+def parse_page(data: dict): # функция парсит непостредственно старничку
+    vacancies = {}
+    items = data['items']
+    for i in items:
+        vacancies[i['id']] = i['name']
+    return vacancies
+
+def main(): # главная ф-ция, в которой вызываются предыдущие ф-ции
+    vacancies = []
+    url_api = "https://api.hh.ru/vacancies"
+    pages, found = get_vacancies_pages(url=url_api)
+    print(f'Найдено {found} вакансий')
+    for i in range(1, 3):
+        data_page = get_data_page(url=url_api, page=i)
+        vacancies.append(parse_page(data_page))
+    print(vacancies, sep='/n')
 
 
-
-url_api = "https://api.hh.ru/vacancies"
-get_vacancies_data(url=url_api)
+if __name__ == '__main__':
+    main()
