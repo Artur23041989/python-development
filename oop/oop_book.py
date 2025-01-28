287e43af8c0f769fee2ce7fe9904d755994d2a8c
Artur23041989 <danh1989@mail.ru>
2025-01-28 21:56:23 +0300

    api_library

---
+++ oop/oop_book.py
@@ -0,0 +1,26 @@
+
+
+
+
+class Book:
+
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def get_info(self):
+        return (f'{self.title} - {self.author}')
+
+    def is_older_than(self, year):
+
+    def update_year(self, new_year):
+
+    def set_genre(self, genre):
+
+    def set_isbn(self, isbn):
+
+    def get_age(self):
+
+    def compare_books(self, other_book):
+
